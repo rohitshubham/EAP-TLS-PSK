685d92a3c46eb297bd75bbe72e20e15d60977f71
Rohit Raj <rohit.raj@alto.fi>
2020-02-26 16:37:49 +0200

    Added build response to tls-psk

--- src/eap_server/eap_server_tls_psk.c
+++ src/eap_server/eap_server_tls_psk.c
@@ -14,6 +14,7 @@ struct eap_tls_psk_data {
 	u8 eap_type;
 };
 
+// Start Section: Common Methods. ToDo should be refactored
 static void eap_tls_reset(struct eap_sm *sm, void *priv)
 {
 	struct eap_tls_psk_data *data = priv;
@@ -22,6 +23,34 @@ static void eap_tls_reset(struct eap_sm *sm, void *priv)
 	eap_server_tls_ssl_deinit(sm, &data->ssl);
 	os_free(data);
 }
+
+static const char * eap_tls_state_txt(int state)
+{
+	switch (state) {
+	case START:
+		return "START";
+	case CONTINUE:
+		return "CONTINUE";
+	case SUCCESS:
+		return "SUCCESS";
+	case FAILURE:
+		return "FAILURE";
+	default:
+		return "Unknown?!";
+	}
+}
+
+
+static void eap_tls_state(struct eap_tls_psk_data *data, int state)
+{
+	wpa_printf(MSG_DEBUG, "EAP-TLS-PSK: %s -> %s",
+		   eap_tls_state_txt(data->state),
+		   eap_tls_state_txt(state));
+	data->state = state;
+	if (state == FAILURE)
+		tls_connection_remove_session(data->ssl.conn);
+}
+// End Section: Common Methods.
 
 
 static void * eap_tls_psk_init(struct eap_sm *sm)
@@ -40,7 +69,7 @@ static void * eap_tls_psk_init(struct eap_sm *sm)
 		return NULL;
 	}
 
-	data->eap_type = EAP_TYPE_TLS;
+	data->eap_type = EAP_TYPE_TLS_PSK;
 	wpa_printf(MSG_INFO, "EAP-TLS-PSK: This is working now!.");
 	return data;
 
@@ -48,10 +77,39 @@ static void * eap_tls_psk_init(struct eap_sm *sm)
 
 static void eap_tls_psk_reset(struct eap_sm *sm, void *priv)
 {
+	struct eap_tls_psk_data *data = priv;
+	if (data == NULL)
+		return;
+	eap_server_tls_ssl_deinit(sm, &data->ssl);
+	os_free(data);
 }
 
-static struct wpabuf * eap_tls_psk_buildReq(struct eap_sm *sm, void *priv)
+static struct wpabuf * eap_tls_psk_req_build(struct eap_sm *sm,
+					   struct eap_tls_psk_data *data, u8 id)
 {
+	struct wpabuf *req;
+	req = eap_tls_msg_alloc(data->eap_type, 1, EAP_CODE_REQUEST, id);
+
+	if(req == NULL) 
+	{
+		wpa_printf(MSG_ERROR, "EAP-TLS-PSK: Failed to allocate memory for request");
+		return NULL;
+	}
+
+	wpabuf_put_u8(req, EAP_TLS_FLAGS_START);
+
+	eap_tls_state(data, CONTINUE);
+
+	return req;
+
+}
+
+static struct wpabuf * eap_tls_psk_buildReq(struct eap_sm *sm, void *priv, u8 id)
+{
+	struct eap_tls_psk_data *data = priv;
+	struct wpabuf *res;
+
+	return eap_tls_psk_req_build(sm, data, id);
 }
 
 static void eap_tls_psk_check(struct eap_sm *sm, void *priv)
